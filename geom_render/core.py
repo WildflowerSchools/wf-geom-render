8b4aa5cd191557631ae548e01835310d1bc9c91a
Theodore Quinn <tcquinn@tcquinn.org>
2019-12-15 21:44:39 -0600

    Add two basic geoms

--- geom_render/core.py
+++ geom_render/core.py
@@ -8,3 +8,25 @@ class Geom:
     ):
     self.color = color
     self.alpha = alpha
+
+class Circle(Geom):
+    def __init__(
+    self,
+    radius=1,
+    color,
+    alpha
+    ):
+    self.radius = radius
+    super().__init__(color, alpha)
+
+class Line(Geom):
+    def __init__(
+    self,
+    thickness=1,
+    line_type='solid',
+    color,
+    alpha
+    ):
+    self.thickness = thickness
+    self.line_type = line_type
+    super().__init__(color, alpha)
