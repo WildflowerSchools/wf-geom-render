2d28d6e6b9cd7419af0f131c80ff5bdcad99c1ea
Theodore Quinn <tcquinn@tcquinn.org>
2019-12-16 08:18:12 -0600

    Fix typo in dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
 
 # Dependencies (format is 'PYPI_PACKAGE_NAME[>=]=VERSION_NUMBER')
 BASE_DEPENDENCIES = [
-    'wf-cv-utils>=0.1.0'
+    'wf-cv-utils>=0.1.0',
     'numpy>=1.17'
 ]
 
